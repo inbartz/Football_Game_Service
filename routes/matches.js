9974cb95f7f5c935a984895e07c693daa5307b9a
inbartz <inbartz@post.bgu.ac.il>
2021-08-07 19:39:08 +0300

    fix feature - get matches bt team and status

--- routes/matches.js
+++ routes/matches.js
@@ -4,28 +4,24 @@ var router = express.Router();
 
 router.get('/team', async (req, res, next)=> {
     let team_name = req.query.team_name;
-    console.log("The search team name is:" + team_name);
-    try{
-        var result = await DButils.GetMatchesByTeam(team_name);
-        res.status(200).send(result);
-    }
-    catch(error){
-        next(error);
-      } 
-})
-
-
-router.get('/status', async (req, res, next)=> {
     let status = req.query.status;
-    console.log("The search matches status is:" + status);
     try{
-        var result = await DButils.GetMatchesByStatus(status);
-        res.status(200).send(result);
+        if(status===undefined && team_name!=undefined){
+            console.log("The search matches tournament is:" + team_name);
+            var result = await DButils.GetMatchesByTeam(team_name);
+            res.status(200).send(result);
+        }
+        else if(status!=undefined && team_name!=undefined){
+            console.log("The search matches team_name is: " + team_name+ " and the matches status is: "+status);
+            var result = await DButils.GetMatchesByTeamAndStatus(team_name, status);
+            res.status(200).send(result);
+        }
     }
     catch(error){
         next(error);
       } 
 })
+
 
 router.get('/tournament', async (req, res, next)=> {
     let tournament = req.query.tournament;
--- routes/utils/DButils.js
+++ routes/utils/DButils.js
@@ -12,17 +12,17 @@ function readCSV(path, status) {
         .on('data', function (row) {
             count++;
             if (status=='played'){
-              entries.push(new Entry_Played(row[0], row[1], row[2], row[3], row[4], row[5],status))
+              entries.push(new Match_Played(row[0], row[1], row[2], row[3], row[4], row[5],status))
             }
             else if (status=='upcoming'){
-              entries.push(new Entry_Upcoming(row[0], row[1], row[2], row[3], row[4], status))
+              entries.push(new Match_Upcoming(row[0], row[1], row[2], row[3], row[4], status))
             }
             
         })
     return entries;
 }
 
-class Entry_Played {
+class Match_Played {
     constructor(home_team, home_score, away_team, away_score, tournament, start_time, status) {
         this.home_team = home_team;
         this.home_score = home_score;
@@ -33,7 +33,7 @@ class Entry_Played {
         this.status = status;
     }
 }
-class Entry_Upcoming {
+class Match_Upcoming {
   constructor(home_team, away_team, tournament, start_time, kickoff, status) {
       this.home_team = home_team;
       this.away_team = away_team;
@@ -47,38 +47,40 @@ class Entry_Upcoming {
 async function GetMatchesByTeam(team_name){
   var found_played = played_matches.filter(element => element.home_team===team_name || element.away_team===team_name);
   var found_upcoming = upcoming_matches.filter(element => element.home_team===team_name || element.away_team===team_name);
-  const found = {team_name: found_played.concat(found_upcoming)}
+  const found = {matches: found_played.concat(found_upcoming)}
   return found; 
 };
 
-async function GetMatchesByStatus(status){
+async function GetMatchesByTeamAndStatus(team_name, status){
   if(status==='played'){
-    return {status: played_matches};
+    var found_played = {matches: played_matches.filter(element => element.home_team===team_name || element.away_team===team_name)}
+    return found_played;
   }
   else if(status==='upcoming'){
-    return {status: upcoming_matches};
+    var found_upcoming = {matches: upcoming_matches.filter(element => element.home_team===team_name || element.away_team===team_name)}
+    return found_upcoming;
   }
-}
+};
 
 async function GetMatchesByTournament(tournament){
   var found_played = played_matches.filter(element => element.tournament===tournament);
   var found_upcoming = upcoming_matches.filter(element => element.tournament===tournament);
-  const found = {tournament : found_played.concat(found_upcoming)}
+  const found = {matches : found_played.concat(found_upcoming)}
   return found; 
 };
 
 async function GetMatchesByTournamentAndStatus(tournament, status){
   if(status==='played'){
-    var found_played = {tournament: {status: played_matches.filter(element => element.tournament===tournament)}}
+    var found_played = {matches: played_matches.filter(element => element.tournament===tournament)}
     return found_played;
   }
   else if(status==='upcoming'){
-    var found_upcoming = {tournament: {status: upcoming_matches.filter(element => element.tournament===tournament)}}
+    var found_upcoming = {matches: upcoming_matches.filter(element => element.tournament===tournament)}
     return found_upcoming;
   }
 };
 
 exports.GetMatchesByTeam = GetMatchesByTeam;
-exports.GetMatchesByStatus = GetMatchesByStatus;
+exports.GetMatchesByTeamAndStatus = GetMatchesByTeamAndStatus;
 exports.GetMatchesByTournament = GetMatchesByTournament;
 exports.GetMatchesByTournamentAndStatus = GetMatchesByTournamentAndStatus;
